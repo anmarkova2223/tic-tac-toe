3a3e9d16ac8d552fdac07f957462dcdf008d7b48
anmarkova2223 <natya.markova@gmail.com>
2023-01-03 16:39:11 -0600

    genius computer added

--- player.py
+++ player.py
@@ -39,9 +39,51 @@ class HumanPlayer(Player):
                 print("Invalid square. Try again.")
         return val
 
+
 class GeniusComputer(Player):
     def __int__(self, letter):
-        super().__init(letter)
+        super().__init__(self, letter)
 
-    def get_move(self):
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = random.choice(game.available_moves())
+        else:
+            # get square based off of minimax algorithm
+            square = self.minimax(game, self.letter)['position']
+        return square
 
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player = "0" if player == "X" else "X"
+
+        if state.current_winner == other_player:
+            return {'position': None,
+                    'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (
+                                state.num_empty_squares() + 1)
+                    }
+        elif not state.empty_squares():
+            return {'position': None, 'score': 0}
+        # starter dictionaries
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}
+        else:
+            best = {'position': None, 'score': math.inf}
+        for possible_move in state.available_moves():
+            # step 1: make a move and try that spot
+            state.make_move(possible_move, player)
+
+            # step 2: recurse
+            sim_score = self.minimax(state, other_player)
+
+            # step 3: undo that move to try others
+            state.board[possible_move] = ' '
+            state.current_winner = None
+            sim_score['position'] = possible_move
+            # step 4: update dictionaries if necessary
+            if player == max_player:
+                if  sim_score['score'] > best['score']:
+                    best = sim_score
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+        return best
